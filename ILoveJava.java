82e0b135fe4bcbb87060cd7ce4b2eb9065167838
hazanisuhairy <115607585+hazanisuhairy@users.noreply.github.com>
2022-10-14 10:50:11 +0800

    Add files via upload

---
+++ ILoveJava.java
@@ -0,0 +1,13 @@
+package LAB1;
+
+public class ILoveJava {
+	public static void main(String[]args) {
+	  System.out.println("I love Java");
+	  System.out.println("I love Java");
+	  System.out.println("I love Java");
+	  System.out.println("I love Java");
+	  System.out.println("I love Java");
+	 
+	}
+
+}
